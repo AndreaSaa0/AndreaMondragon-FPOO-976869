ef69695cf38f3b41507f3309e1dd255779135f7e
Andrea Mondragon <andrea.mondragon@uniminuto.edu.co>
2024-09-26 01:42:13 +0000

    mensajelo que faltaba de las clases y demás

--- SemanaTres/class_person.py
+++ SemanaTres/class_person.py
@@ -16,8 +16,14 @@ class Person:
         self.role = role
 
     def __repr__ (self):
-        return f"{self.name} {self.last_name}"
+        return f"{self.dni} {self.name} {self.last_name} {self.role}"
 
 person1 = Person(dni = 123, name="Luisito", lastname = "Velez", role=1)
+person2 = Person(dni = 456, name="Andrea", lastname = "Saa", role=2)
+person3 = Person(dni = 789, name="Diana", lastname = "Hernandez", role=3)
+person4 = Person(dni = 101112, name="David", lastname = "Castro", role=4)
 
 print(person1)
+print(person2)
+print(person3)
+print(person4)
